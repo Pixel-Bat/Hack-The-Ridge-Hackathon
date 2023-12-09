105bd2d30087fb1353de9f3553571dbc52cc99a8
Mekyle-Hemani <153373890+Mekyle-Hemani@users.noreply.github.com>
2023-12-09 12:40:48 -0500

    Update test.py

--- test.py
+++ test.py
@@ -2,9 +2,10 @@ import folium
 import os, sys, subprocess
 from pathlib import Path
 import platform
+from countryinfo import CountryInfo
 
 filelocation = "interactive_map.html"
-
+''''''
 whatOS = platform.system()
 
 if whatOS == "Darwin":
@@ -45,6 +46,34 @@ if whatOS == "Darwin":
     command = (f"open -a 'Google Chrome' {filelocation}")
     subprocess.Popen(command, shell=True)
 
+    #while True:
+
 if whatOS == "Windows":
-    print("OS is windows")
-    exit()
+
+    print("OS is Windows")
+
+    my_file = Path(filelocation)
+    if my_file.is_file():
+        fileExist = 1
+    else:
+        fileExist = 0
+
+    if fileExist == 1:
+        os.remove(".\interactive_map.html")
+
+    # Create a base map centered at a specific location
+    m = folium.Map(location=[37.7749, -122.4194], zoom_start=12)
+
+    # Add a marker to the map
+    folium.Marker(
+        location=[37.7749, -122.4194],
+        popup="San Francisco",
+        icon=folium.Icon(color="blue"),
+    ).add_to(m)
+
+        # Save the map as an HTML file
+    m.save(filelocation)
+
+    os.system(".\interactive_map.html")
+
+    #while True:
