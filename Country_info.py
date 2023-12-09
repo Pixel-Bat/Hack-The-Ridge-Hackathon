8f07fa37eccc4b2f9b36f03c3b5b73deb8ff71d7
Mekyle-Hemani <153373890+Mekyle-Hemani@users.noreply.github.com>
2023-12-09 12:47:11 -0500

    Update Country_info.py

---
+++ Country_info.py
@@ -0,0 +1,39 @@
+from countryinfo import CountryInfo
+
+
+# Input the Name of the Country
+name=input("Enter your country: ")
+
+# Passing the country name to the in-built function of the module
+country = CountryInfo(name)
+
+# Printing all the details using the Pre-defined Functions
+
+# 1) Printing the Capital of the Country
+print("Capital is: ",country.capital())
+
+# 2) Printing the Currency used in the Country
+print("Currencies is: ",country.currencies())
+
+# 3) Printing the Languages spoken in the Country
+print("Language is: ",country. languages())
+
+# 4) Printing the Borders of the Country and Function
+# returns bordering countries (ISO3) for a specified country
+print("Borders are: ",country.borders())
+
+# 5) Printing the Provinces of the Country
+print("Provinces are: ", country.provinces())
+
+# 8) Printing the Latitudes and Longitudes of the Country
+print("Capital Latitudes and Longitudes are: ", 
+						country.capital_latlng())
+
+# 9) Printing the TimeZone in the Country
+print("TimeZone: ", country.timezones())
+
+# 10) Printing the Current Population in the Country
+print("Population: ", country.population())
+
+# 11) Printing the Other Names of the Country
+print("Others names: ",country.alt_spellings())
